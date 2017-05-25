39998f3c179ffed22dc00e916180b4344f3a63c0
doyoon Kim <dkim0718@gmail.com>
2017-05-24 20:56:43 -0400

    Version 0.01

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ A simple python WHOIS parser
 
 "Noogu" means "who" in Korean. It is a simple Python parser for WHOIS queries.
 
-More to come.
+Instead of relying too heavily on having too many regexes, noogu tries to 
+emulate a person reading actual whois queries. It will try to identify whether
+the line currently being read fits under tech, admin, or registrant. Then it
+will try to figure out what about the tech/admin/registrant the line is trying
+to say (e.g., name, etc)
+
+Usage:
+noogu(text)
--- noogu/noogu.py
+++ noogu/noogu.py
@@ -11,47 +11,21 @@ def lines_from(text):
 	lines = [line for line in lines if not line.startswith('>>>')]
 	return lines
 
-def tweak_keys(section,subsection):
-	""" Return adjusted section and key names 
+def tweak_keys(section,candidate):
+	""" Guess the appropriate result key name 
 
-	There are plans to make this into one big dictionary,
-	which you can see here 
-
+	This will update the 
 	Need to add: Subsection may take precedence over section 
 	"""
 	# Large dictionaries
 	section_dict = {
-		# co.jp
-		# 'Domain Information': 'Registrant',
-
-		# .sg
-		# 'Administrative Contact': 'admin',
-		# 'Technical Contact': 'tech',
-		# 'Name Servers': 'nameservers',
-
-		# # .de
-		# 'Tech-C': 'tech',
-		# 'Zone-C': 'registrant',
-
-		# # .eu
-		# 'Technical': 'tech',
-		# 'Name servers': 'nameservers',
-
 		# .fr
 		'tech-c':'tech',
 		'holder-c':'registrant',
-		'admin-c':'admin',
-
-		# # .com
-
-		# # .edu
-		# 'Administrative Contact':'admin',
-		# 'Registrant':'registrant',
-		# 'Technical Contact':'tech'
-
+		'admin-c':'admin'
 	}
 
-	subsection_dict = {
+	candidate_dict = {
 		'Nserver':'nameserver',
 		
 		# co.jp
@@ -63,58 +37,73 @@ def tweak_keys(section,subsection):
 
 	# Section adjustments
 	if section == 'Domain Information':
+		status = 'parsing with respect to '
 		# co.jp 
-		if re.search(r'Admin',subsection,re.IGNORECASE):
-			new_section = 'admin'
-		if re.search(r'Technical',subsection,re.IGNORECASE):
-			new_section = 'tech'
+		if re.search(r'Admin',candidate,re.IGNORECASE):
+			new_candidate = 'admin'
+		if re.search(r'Technical',candidate,re.IGNORECASE):
+			new_candidate = 'tech'
 		else:
-			new_section = 'registrant'
+			new_candidate = 'registrant'
+
 
 	elif section in list(section_dict):
-		new_section = section_dict.get(section)
+		status = 'exception cases'
+		new_candidate = section_dict.get(section)
+
 
 	elif section:
+		status = 'section is specified'
 		if re.search(r'admin',section,re.IGNORECASE):
-			new_section = 'admin'
+			new_candidate = 'admin'
 		elif re.search(r'tech',section,re.IGNORECASE):
-			new_section = 'tech'
+			new_candidate = 'tech'
 		elif re.search(r'name ?server',section,re.IGNORECASE):
-			new_section = 'nameservers'
+			new_candidate = 'nameservers'
 		elif re.search(r'registrant',section,re.IGNORECASE):
-			new_section = 'registrant'
+			new_candidate = 'registrant'
 		elif re.search(r'registrar',section,re.IGNORECASE):
-			new_section = 'registrar'
-	else:
-		# Check subsection
-		if re.search(r'admin',subsection,re.IGNORECASE):
-			new_section = 'admin'
-		elif re.search(r'tech',subsection,re.IGNORECASE):
-			new_section = 'tech'
-		elif re.search(r'registrant',subsection,re.IGNORECASE):
-			new_section = 'registrant'
-		elif re.search(r'registrar',subsection,re.IGNORECASE):
-			new_section = 'registrar'
+			new_candidate = 'registrar'
 		else:
-			new_section = section
+			status = 'section specified but not sure what it is'
+			print(section)
+			new_candidate = section
 
-	# subsection adjustments
-	if re.search(r'organization|organisation|contact',subsection,re.IGNORECASE):
-		if re.search(r'type',subsection,re.IGNORECASE):
-			new_subsection = 'org_type'
-		else:
-			new_subsection = 'org'
-	elif re.search(r'name|person', subsection,re.IGNORECASE):
-		if re.search(r'server',subsection,re.IGNORECASE):
-			new_subsection = 'nameserver'
-		else:
-			new_subsection = 'name'
-	elif re.search(r'address', subsection,re.IGNORECASE):
-		new_subsection = 'address'
 	else:
-		new_subsection = subsection
+		status = 'section == None'
+		new_candidate = '' 
+		# Check candidate
+		if re.search(r'admin',candidate,re.IGNORECASE):
+			new_candidate = 'admin'
+		elif re.search(r'tech',candidate,re.IGNORECASE):
+			new_candidate = 'tech'
+		elif re.search(r'registrant',candidate,re.IGNORECASE):
+			new_candidate = 'registrant'
+		elif re.search(r'registrar',candidate,re.IGNORECASE):
+			new_candidate = 'registrar'
+		else:
+			new_candidate = ''
 
-	return new_section, new_subsection
+	# Extract our new candidate from input candidate
+	candidate = re.sub(r'{}(.*?)'.format(new_candidate),r'\1', candidate, flags=re.I).strip()
+
+	# Match the remaining candidate input
+	if re.search(r'organization|organisation|contact',candidate,re.IGNORECASE):
+		if re.search(r'type',candidate,re.IGNORECASE):
+			new_candidate = ' '.join([new_candidate,'org_type'])
+		else:
+			new_candidate = ' '.join([new_candidate,'org'])
+	elif re.search(r'name|person', candidate,re.IGNORECASE):
+		if re.search(r'server',candidate,re.IGNORECASE):
+			new_candidate = ' '.join([new_candidate,'nameserver'])
+		else:
+			new_candidate = ' '.join([new_candidate,'name'])
+	elif re.search(r'address', candidate,re.IGNORECASE):
+		new_candidate = ' '.join([new_candidate,'address'])
+	else:
+		new_candidate = ' '.join([new_candidate,candidate])
+
+	return new_candidate.lower().strip()
 
 def guess_buffer(lines):
 	# sanitize
@@ -161,73 +150,87 @@ def guess_buffer(lines):
 	return result
 
 
-def noogu(text):
-	SECTION = None
+def noogu(text, verbose=False):
+	# TODO : 
+	# 1. Need to specify when subsection can override sections
+	status = None
+	section = None
 	LOOKING = None
 	section_buffers = {}
-	debug = {}
-	inv_debug = {}
+	result = {}
+	inv_result = {}
 	prev_line_blank = False
 
 	lines = lines_from(text)
 	for line in lines:
-		if prev_line_blank: print("Previous line blank")
-		line=line.strip()
-		# Sections carry over multiple lines
+		# Section carries over lines and changes only when previous line is 
+		# blank, followed by something like "Registrar:"
+		# if prev_line_blank: print("Previous line blank")
+		line = line.strip() # Do you need this? 
 		new_section = re.search(r'(.*?):$',line)
 		if new_section:
 			if prev_line_blank:
-				SECTION = new_section.group(1)
-				print("NEW SECTION")
+				status = 'found new section'
+				section = new_section.group(1)
+				print("NEW section")
 			else:
-				lkey = new_section.group(1)
-				lvalue = None
+				status = 'found empty value'
+				result_key = new_section.group(1)
+				result_value = None
 
 
 		# Check if there is a : followed by spaces
 		LOOKING = re.search(r'(.*?):\s+(.*?)$',line)
 		if LOOKING:
-			lkey = LOOKING.group(1)
-			lvalue = LOOKING.group(2)
+			status = 'found subsection and value'
+			result_key = LOOKING.group(1)
+			result_value = LOOKING.group(2)
 
 
 			# Check if the key values match any other key values
-			if lvalue in list(debug.values()):
+			if result_value in list(result.values()):
 				# For FRNIC type whois queries 
-				if '-FRNIC' in lvalue:
-					SECTION = inv_debug[lvalue]
+				if '-FRNIC' in result_value:
+					section = inv_result[result_value]
 
 			# Standardize the keys
-			SECTION, lkey = tweak_keys(SECTION, lkey)
-
+			#------------------------------------------------------------------
+			#
+			# This part needs a lot of rethinking
+			# 
+			# All we need here is to update the result key using the section and result key
+			result_key = tweak_keys(section, result_key) 
+			VALUE = result_value
 
 			# Name the sections appropriately
-			if SECTION:
-				KEY = '{}_{}'.format( SECTION, lkey )
-				VALUE = lvalue
-			else:
-				KEY = lkey
-				VALUE = lvalue
-
-
+			# if section:
+			# 	status = 'updating result_key'
+			# 	result_key = '{}_{}'.format( section, result_key )
+			# 	VALUE = result_value
+			# else:
+			# 	status = 'no need to update result key'
+			# 	result_key = result_key
+			# 	VALUE = result_value
+			#
+			#------------------------------------------------------------------
 			# If the same key already exists, append
-			if KEY in list(debug):
-				VALUE = ';'.join([debug[KEY],VALUE])
+			if result_key in list(result):
+				VALUE = ';'.join([result[result_key],VALUE])
 
 			# Add to data 
-			debug[KEY] = VALUE
-			inv_debug[VALUE] = KEY 
+			result[result_key] = VALUE
+			inv_result[VALUE] = result_key 
 
 		# If no : (e.g., .edu domains)
 		# Need to guess what these correspond to
-		elif SECTION!=None:
-			SECTION, _throwaway_ = tweak_keys(SECTION,'')
-			if SECTION in list(section_buffers):
-				section_buffers[SECTION].append(line.strip())
+		elif section!=None:
+			section, _throwaway_ = tweak_keys(section,'')
+			if section in list(section_buffers):
+				section_buffers[section].append(line.strip())
 			else:
-				section_buffers[SECTION] = [line]
+				section_buffers[section] = [line]
 		try:
-			print('{}, {}, {}'.format(SECTION,lkey,lvalue))
+			print('{}, {}, {}'.format(section,result_key,result_value))
 		except:
 			print(line)
 
@@ -242,136 +245,139 @@ def noogu(text):
 		for section in list(section_buffers):
 			if not re.search(r'name ?server',section,re.IGNORECASE):
 				guess = guess_buffer(section_buffers[section])
-				for subsection in list(guess):
-					debug['{}_{}'.format(section,subsection)] = guess[subsection]
-	return debug
+				for result_key in list(guess):
+					result['{}_{}'.format(section,result_key)] = guess[result_key]
+
+	# Add the text to result
+	result['raw'] = text
+	return result
 
 
 
-def hint_from(line):
-	if re.search(r'organization|organisation',line,re.IGNORECASE):
-		hint = 'org'
-	elif re.search(r'name|person',line,re.IGNORECASE):
-		hint = 'name'
-	else: 
-		hint = None 
-	return hint
+# def hint_from(line):
+# 	if re.search(r'organization|organisation',line,re.IGNORECASE):
+# 		hint = 'org'
+# 	elif re.search(r'name|person',line,re.IGNORECASE):
+# 		hint = 'name'
+# 	else: 
+# 		hint = None 
+# 	return hint
 
 
-def whois_from_text(text):
-	"""
-	When we read whois queries, we first start by ignoring all the information
-	then once we reach something like "tech" or "domain information". This 
-	toggles the 'LOOKING_FOR' variable
+# def whois_from_text(text):
+# 	"""
+# 	When we read whois queries, we first start by ignoring all the information
+# 	then once we reach something like "tech" or "domain information". This 
+# 	toggles the 'LOOKING_FOR' variable
 
-	CURR_SECTION: which section are we currently on? (e.g., admin org)
-	PREV_SECTION: which section were we on previously?
-	LINE_SECTION: 
-	HINT: Does the current line contain 'org' or 'name' or 'address?'
+# 	CURR_section: which section are we currently on? (e.g., admin org)
+# 	PREV_section: which section were we on previously?
+# 	LINE_section: 
+# 	HINT: Does the current line contain 'org' or 'name' or 'address?'
 
-	"""
-	CURR_SECTION = 'Start'
-	PREV_SECTION = 'Start'
-	# What you're looking for carries over within SECTIONs but not across
-	LOOKING_FOR = None
-	HINT = None
-	hints = []
-	buffers = {}
-	result = {}
-	section_buffer = []
+# 	"""
+# 	CURR_section = 'Start'
+# 	PREV_section = 'Start'
+# 	# What you're looking for carries over within sections but not across
+# 	LOOKING_FOR = None
+# 	HINT = None
+# 	hints = []
+# 	buffers = {}
+# 	result = {}
+# 	section_buffer = []
 
-	# text = print_whois(splt_alph)
-	lines = lines_from(text)
+# 	# text = print_whois(splt_alph)
+# 	lines = lines_from(text)
 
-	for line in lines:
-		splt_colon = line.split(':')[-1].strip()
+# 	for line in lines:
+# 		splt_colon = line.split(':')[-1].strip()
 		
 
-		# If line contains "Registrar", store that
-		if re.search(r'Registrar:',line,re.IGNORECASE):
-			result['registrar'] = splt_colon
+# 		# If line contains "Registrar", store that
+# 		if re.search(r'Registrar:',line,re.IGNORECASE):
+# 			result['registrar'] = splt_colon
 
 
-		# Line IDs do not carry over
-		LINE_SECTION = None 
-		# HINTs tell us if the line has "organization" or "name" in them
-		HINT = hint_from(line) 
-		# Get current line status
-		for sid in ['Admin','Tech','Registrant','Registrar']:
-			if re.search(r'{}'.format(sid),line):
-				LINE_SECTION = sid.lower()
+# 		# Line IDs do not carry over
+# 		LINE_section = None 
+# 		# HINTs tell us if the line has "organization" or "name" in them
+# 		HINT = hint_from(line) 
+# 		# Get current line status
+# 		for sid in ['Admin','Tech','Registrant','Registrar']:
+# 			if re.search(r'{}'.format(sid),line):
+# 				LINE_section = sid.lower()
 
-		# If detect a line, determine whether to change section
-		if LINE_SECTION == None:
-			# Since section hasn't changed 
-			# Should we do something different here? 
-			section_buffer.append(line)
-		elif (LINE_SECTION == CURR_SECTION):
-			# Section hasn't changed
-			section_buffer.append(line)
-		elif (LINE_SECTION != CURR_SECTION):
-			# Section has changed
-			# Dump buffer
-			buffers[CURR_SECTION] = section_buffer
-			section_buffer = []
+# 		# If detect a line, determine whether to change section
+# 		if LINE_section == None:
+# 			# Since section hasn't changed 
+# 			# Should we do something different here? 
+# 			section_buffer.append(line)
+# 		elif (LINE_section == CURR_section):
+# 			# Section hasn't changed
+# 			section_buffer.append(line)
+# 		elif (LINE_section != CURR_section):
+# 			# Section has changed
+# 			# Dump buffer
+# 			buffers[CURR_section] = section_buffer
+# 			section_buffer = []
 
-			# Update the where we are
-			PREV_SECTION = CURR_SECTION
-			CURR_SECTION = LINE_SECTION.lower()
+# 			# Update the where we are
+# 			PREV_section = CURR_section
+# 			CURR_section = LINE_section.lower()
 
-			# Clear what we're looking for 
-			LOOKING_FOR = None
+# 			# Clear what we're looking for 
+# 			LOOKING_FOR = None
 
-			# If sections changed and no information
-			try:
-				any_info = any([PREV_SECTION in x for x in list(result)])
-				info_len = len(buffers[PREV_SECTION])
-				if (info_len >= 2) & (not any_info) & (PREV_SECTION!='Start'):
-					result['{}_name'.format(PREV_SECTION)] = buffers[PREV_SECTION][0]
-					result['{}_org'.format(PREV_SECTION)] = buffers[PREV_SECTION][1]
-				elif (info_len < 2) & (PREV_SECTION!='Start')&(not any_info):
-					result['{}_name'.format(PREV_SECTION)] = result.get('Start_name',pd.np.nan)
-					result['{}_org'.format(PREV_SECTION)] = result.get('Start_org',pd.np.nan)
-
-
-			except Exception as e:
-				pass
-				# print(str(e))
-				# print(buffers[PREV_SECTION])
-				# print(PREV_SECTION)
-
-		# Within a section, we are looking for the organization and the name
-		# If hint_from is triggered but returns blank, keep looking
-		if (hint_from(line)!=None) & (splt_colon==''):
-			LOOKING_FOR = hint_from(line)
-		# If hint_from is triggered and returns non blanks, stop looking
-		elif (hint_from(line)!=None) & (splt_colon!=''):
-			LOOKING_FOR = hint_from(line)
-			k = '{}_{}'.format(CURR_SECTION,LOOKING_FOR)
-			if k in list(result):
-				pass
-			else:
-				result[k] = splt_colon
-			LOOKING_FOR = None
-		# If hint from is not triggered but is looking for 
-		elif (hint_from(line)==None):
-			if LOOKING_FOR:
-				k = '{}_{}'.format(CURR_SECTION,LOOKING_FOR)
-				if k in list(result):
-					pass
-				else:
-					result[k] = splt_colon
-			else:
-				pass		
-
-		# Remove 'Start' keys
-		for key in list(result):
-			if 'Start' in key:
-				del(result[key])
+# 			# If sections changed and no information
+# 			try:
+# 				any_info = any([PREV_section in x for x in list(result)])
+# 				info_len = len(buffers[PREV_section])
+# 				if (info_len >= 2) & (not any_info) & (PREV_section!='Start'):
+# 					result['{}_name'.format(PREV_section)] = buffers[PREV_section][0]
+# 					result['{}_org'.format(PREV_section)] = buffers[PREV_section][1]
+# 				elif (info_len < 2) & (PREV_section!='Start')&(not any_info):
+# 					result['{}_name'.format(PREV_section)] = result.get('Start_name',pd.np.nan)
+# 					result['{}_org'.format(PREV_section)] = result.get('Start_org',pd.np.nan)
 
 
-		# For testing. 
-		hints.append((CURR_SECTION,LOOKING_FOR))
-	# For testing 
-	# result['splt_alph'] = splt_alph
-	return result
+# 			except Exception as e:
+# 				pass
+# 				# print(str(e))
+# 				# print(buffers[PREV_section])
+# 				# print(PREV_section)
+
+# 		# Within a section, we are looking for the organization and the name
+# 		# If hint_from is triggered but returns blank, keep looking
+# 		if (hint_from(line)!=None) & (splt_colon==''):
+# 			LOOKING_FOR = hint_from(line)
+# 		# If hint_from is triggered and returns non blanks, stop looking
+# 		elif (hint_from(line)!=None) & (splt_colon!=''):
+# 			LOOKING_FOR = hint_from(line)
+# 			k = '{}_{}'.format(CURR_section,LOOKING_FOR)
+# 			if k in list(result):
+# 				pass
+# 			else:
+# 				result[k] = splt_colon
+# 			LOOKING_FOR = None
+# 		# If hint from is not triggered but is looking for 
+# 		elif (hint_from(line)==None):
+# 			if LOOKING_FOR:
+# 				k = '{}_{}'.format(CURR_section,LOOKING_FOR)
+# 				if k in list(result):
+# 					pass
+# 				else:
+# 					result[k] = splt_colon
+# 			else:
+# 				pass		
+
+# 		# Remove 'Start' keys
+# 		for key in list(result):
+# 			if 'Start' in key:
+# 				del(result[key])
+
+
+# 		# For testing. 
+# 		hints.append((CURR_section,LOOKING_FOR))
+# 	# For testing 
+# 	# result['splt_alph'] = splt_alph
+# 	return result
